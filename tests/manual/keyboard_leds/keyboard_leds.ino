b9d34ac5531cc211e3c8ff3e2d978adc529bbbb9
TANAKA Masayuki <tanakamasayuki@gmail.com>
2026-05-15 14:51:06 +0900

    refactor: キーボードLED制御の関数を整理し、待機処理を改善

--- tests/manual/keyboard_leds/keyboard_leds.ino
+++ tests/manual/keyboard_leds/keyboard_leds.ino
@@ -2,6 +2,11 @@
 
 EspUsbHost usb;
 
+static volatile bool keyboardConnected = false;
+static constexpr uint32_t LED_RETRY_DELAY_MS = 100;
+static constexpr uint32_t LED_SETTLE_DELAY_MS = 300;
+static constexpr uint32_t INITIAL_LED_SETTLE_DELAY_MS = 500;
+
 static void waitForAck()
 {
     while (Serial.read() < 0)
@@ -10,36 +15,53 @@ static void waitForAck()
     }
 }
 
+static void waitForKeyboard()
+{
+    while (!keyboardConnected)
+    {
+        delay(10);
+    }
+    delay(INITIAL_LED_SETTLE_DELAY_MS);
+}
+
+static void setKeyboardLeds(bool numLock, bool capsLock, bool scrollLock)
+{
+    while (!usb.setKeyboardLeds(numLock, capsLock, scrollLock))
+    {
+        delay(LED_RETRY_DELAY_MS);
+    }
+    delay(LED_SETTLE_DELAY_MS);
+}
+
+static void setKeyboardLedsAndPrompt(bool numLock, bool capsLock, bool scrollLock, const char *prompt)
+{
+    setKeyboardLeds(numLock, capsLock, scrollLock);
+    Serial.println(prompt);
+    waitForAck();
+}
+
 void setup()
 {
     Serial.begin(115200);
     delay(5000);
+    usb.onDeviceConnected([](const EspUsbHostDeviceInfo &)
+                          { keyboardConnected = true; });
+    usb.onDeviceDisconnected([](const EspUsbHostDeviceInfo &)
+                             { keyboardConnected = false; });
     usb.begin();
 }
 
 void loop()
 {
-    usb.setKeyboardLeds(true, false, false);
-    Serial.println("NumLock ON");
-    waitForAck();
+    waitForKeyboard();
+    setKeyboardLeds(false, false, false);
 
-    usb.setKeyboardLeds(false, false, false);
-    Serial.println("NumLock OFF");
-    waitForAck();
+    setKeyboardLedsAndPrompt(true, false, false, "NumLock ON");
+    setKeyboardLedsAndPrompt(false, false, false, "NumLock OFF");
 
-    usb.setKeyboardLeds(false, true, false);
-    Serial.println("CapsLock ON");
-    waitForAck();
+    setKeyboardLedsAndPrompt(false, true, false, "CapsLock ON");
+    setKeyboardLedsAndPrompt(false, false, false, "CapsLock OFF");
 
-    usb.setKeyboardLeds(false, false, false);
-    Serial.println("CapsLock OFF");
-    waitForAck();
-
-    usb.setKeyboardLeds(false, false, true);
-    Serial.println("ScrollLock ON");
-    waitForAck();
-
-    usb.setKeyboardLeds(false, false, false);
-    Serial.println("ScrollLock OFF");
-    waitForAck();
+    setKeyboardLedsAndPrompt(false, false, true, "ScrollLock ON");
+    setKeyboardLedsAndPrompt(false, false, false, "ScrollLock OFF");
 }
