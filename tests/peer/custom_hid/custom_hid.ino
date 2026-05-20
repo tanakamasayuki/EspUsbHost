7986b69c93441d8aefec09e49f09e2a169b977e7
TANAKA Masayuki <tanakamasayuki@gmail.com>
2026-05-20 11:53:28 +0900

    feat: HIDレポートディスクリプタのコールバックを追加し、テストケースを更新

--- tests/peer/custom_hid/custom_hid.ino
+++ tests/peer/custom_hid/custom_hid.ino
@@ -7,6 +7,14 @@ void setup()
     Serial.begin(115200);
     delay(500);
 
+    usb.onHIDReportDescriptor([](const EspUsbHostHIDReportDescriptor &descriptor)
+                              { Serial.printf("HID_REPORT iface=%u reported=%u len=%u first=%02x last=%02x\n",
+                                              descriptor.interfaceNumber,
+                                              descriptor.reportedLength,
+                                              descriptor.length,
+                                              descriptor.length > 0 ? descriptor.data[0] : 0,
+                                              descriptor.length > 0 ? descriptor.data[descriptor.length - 1] : 0); });
+
     usb.onHIDInput([](const EspUsbHostHIDInput &input)
                    {
       Serial.printf("CUSTOM len=%u data=", (unsigned)input.length);
